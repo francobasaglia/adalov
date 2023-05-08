8b404ab39714a6b9a22d2b16cd04bee9d35aaec2
Franco Basaglia <francobasaglia94@gmail.com>
2023-05-07 22:31:08 -0300

    Initial project setup.

---
+++ scripts/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Clean dependencies
+rm -rf ./node_modules
+
+# Clean output directories
+rm -rf ./out-pck
+rm -rf ./out-tsc
+
+# Clean test coverage output directory
+rm -rf ./coverage
